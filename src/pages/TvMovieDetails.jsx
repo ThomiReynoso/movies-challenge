9582870eb0a28bf049b864618311d1d976e4c6ed
Thomas Reynoso <thomas@mudafy.com>
2023-04-06 12:43:19 -0300

    add basic grid for detail view

--- src/pages/TvMovieDetails.jsx
+++ src/pages/TvMovieDetails.jsx
@@ -1,8 +1,32 @@
+import { Grid, GridItem, Box } from '@chakra-ui/react'
+
 const TvMovieDetails = () => {
     return (
-        <div>
-            detalle de tv shows & movies
-        </div>
+			<Grid 
+				paddingX={10}
+				paddingY={10}
+				height={"60vh"}
+				gridTemplateColumns={"20% 80%"} gridTemplateRows={"repeat(3, 1fr)"} gap={4} 
+				templateAreas={`
+					"image title"
+					"image extra_info"
+					"image description"
+				`}
+			>
+				<GridItem area={'image'}>
+					imagen
+				</GridItem>
+				<GridItem area={'title'}>
+					tituloo
+				</GridItem>
+				<GridItem area={'extra_info'}>
+					infoo
+				</GridItem>
+				<GridItem area={'description'}>
+					descrip
+				</GridItem>
+				
+			</Grid>
     )
 }
 
